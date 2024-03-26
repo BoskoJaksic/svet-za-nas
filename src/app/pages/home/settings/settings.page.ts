f55cc3c909d8d208b0624821d8a3d5333e02cb79
Mitar Branković <bmitar99@gmail.com>
2024-03-26 14:43:55 +0100

    setting page

--- src/app/pages/home/settings/settings.page.ts
+++ src/app/pages/home/settings/settings.page.ts
@@ -1,12 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {UserService} from "../../../common/services/user.service";
-import {LocalStorageService} from "../../../common/services/local-storage.service";
-import {LoaderService} from "../../../common/services/loader.service";
-import {GoogleAuth} from "@codetrix-studio/capacitor-google-auth";
-import {ToasterService} from "../../../common/services/toaster.service";
-import {CommonService} from "../../../common/services/common.service";
-import {Share} from "@capacitor/share";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../../common/services/user.service';
+import { LocalStorageService } from '../../../common/services/local-storage.service';
+import { LoaderService } from '../../../common/services/loader.service';
+import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
+import { ToasterService } from '../../../common/services/toaster.service';
+import { CommonService } from '../../../common/services/common.service';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-settings',
@@ -15,7 +15,7 @@ import {Share} from "@capacitor/share";
 })
 export class SettingsPage implements OnInit {
   avatarImg: string | undefined;
-  userInfo: any
+  userInfo: any;
   public alertButtons = [
     {
       text: 'Ponisti',
@@ -33,45 +33,45 @@ export class SettingsPage implements OnInit {
     },
   ];
 
-  constructor(private route: ActivatedRoute,
-              private userService: UserService,
-              private loaderService: LoaderService,
-              private toasterService: ToasterService,
-              private commonService: CommonService,
-              private localStorageService: LocalStorageService,
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private loaderService: LoaderService,
+    private toasterService: ToasterService,
+    private commonService: CommonService,
+    private localStorageService: LocalStorageService
   ) {
-    this.avatarImg = 'https://ionicframework.com/docs/img/demos/avatar.svg'
-
+    this.avatarImg = 'https://ionicframework.com/docs/img/demos/avatar.svg';
   }
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.getUserData();
-    })
+    });
   }
 
   getUserData() {
     this.loaderService.showLoader();
-    let email = this.localStorageService.getUserEmail()
+    let email = this.localStorageService.getUserEmail();
     this.userService.getUserDataByEmail(email).subscribe({
       next: (r) => {
-        console.log('r', r)
-        this.userInfo = r.value
+        console.log('r', r);
+        this.userInfo = r.value;
         if (r.value.profilePicture) {
-          this.avatarImg = r.value.profilePicture
+          this.avatarImg = r.value.profilePicture;
         }
         this.loaderService.hideLoader();
-      }, error: (err) => {
+      },
+      error: (err) => {
         this.loaderService.hideLoader();
-
-      }
-    })
+      },
+    });
   }
 
   deleteAccount(ev: any) {
     if (ev.detail.role === 'confirm') {
       this.loaderService.showLoader();
-      let userEmail = this.localStorageService.getUserEmail()
+      let userEmail = this.localStorageService.getUserEmail();
 
       this.userService.deleteAccount(userEmail).subscribe({
         next: (r) => {
@@ -80,15 +80,16 @@ export class SettingsPage implements OnInit {
           this.commonService.goToRoute('');
           this.toasterService.presentToast('Nalog uspesno obrisan', 'success');
           this.loaderService.hideLoader();
-        }, error: (err) => {
+        },
+        error: (err) => {
           this.loaderService.hideLoader();
           this.toasterService.presentToast('Doslo je do greske', 'danger');
-        }
-      })
+        },
+      });
     }
   }
   async addPartner() {
-    let partnerId = this.localStorageService.getUserId()
+    let partnerId = this.localStorageService.getUserId();
     await Share.share({
       title: 'Svet za nas',
       text: 'Hej, zelim da koristis svet za nas aplikaciju zajedno sa mnom',
@@ -97,8 +98,6 @@ export class SettingsPage implements OnInit {
     });
   }
   goBack() {
-    this.commonService.goToRoute('home/profile')
+    this.commonService.goToRoute('home/profile');
   }
-
 }
-
