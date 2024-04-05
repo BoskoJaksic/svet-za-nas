4991308071a0767bec93cdfc6a919b255e4779f1
boskojaksic <bosko.jaksic@wedosoftware.eu>
2024-04-05 13:01:35 +0200

    env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,3 @@ export const environment = {
   production: true,
   baseURL:'https://app-svet-za-nas-backend-staging.azurewebsites.net/api/'
 };
-// baseURL:'https://bebac-backend.azurewebsites.net/api/'
