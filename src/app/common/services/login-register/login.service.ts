5b63924dbf9fce8510a5fd838629d83e2a52aadd
Mitar Branković <bmitar99@gmail.com>
2024-05-14 14:01:22 +0200

    apple login request body changed

--- src/app/common/services/login-register/login.service.ts
+++ src/app/common/services/login-register/login.service.ts
@@ -1,13 +1,12 @@
-import {Injectable} from '@angular/core';
-import {ApiService} from '../../../core/api.service';
-import {Observable} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { ApiService } from '../../../core/api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
-  constructor(private apiService: ApiService) {
-  }
+  constructor(private apiService: ApiService) {}
 
   loginUser(data: any): Observable<any> {
     return this.apiService.post('ApplicationUsers/LoginUser', data);
@@ -18,11 +17,13 @@ export class LoginService {
   }
 
   facebookLoginIn(token: any): Observable<any> {
-    return this.apiService.post(`ApplicationUsers/FacebookLogin/${token}`, token);
+    return this.apiService.post(
+      `ApplicationUsers/FacebookLogin/${token}`,
+      token
+    );
   }
 
-  appleLogin(token: any,email:any): Observable<any> {
-    return this.apiService.post(`ApplicationUsers/AppleLogin/${token}/email`, token);
+  appleLogin(data: any): Observable<any> {
+    return this.apiService.post(`ApplicationUsers/AppleLogin`, data);
   }
-
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,14 +1,14 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {CommonService} from '../../common/services/common.service';
-import {LoginService} from '../../common/services/login-register/login.service';
-import {LocalStorageService} from '../../common/services/local-storage.service';
-import {ToasterService} from '../../common/services/toaster.service';
-import {GoogleAuth} from '@codetrix-studio/capacitor-google-auth';
-import {jwtDecode} from 'jwt-decode';
-import {FacebookLogin,} from '@capacitor-community/facebook-login';
-import {UserService} from "../../common/services/user.service";
-import {HttpClient} from '@angular/common/http';
-import {LoaderService} from "../../common/services/loader.service";
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CommonService } from '../../common/services/common.service';
+import { LoginService } from '../../common/services/login-register/login.service';
+import { LocalStorageService } from '../../common/services/local-storage.service';
+import { ToasterService } from '../../common/services/toaster.service';
+import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
+import { jwtDecode } from 'jwt-decode';
+import { FacebookLogin } from '@capacitor-community/facebook-login';
+import { UserService } from '../../common/services/user.service';
+import { HttpClient } from '@angular/common/http';
+import { LoaderService } from '../../common/services/loader.service';
 // import {Plugins} from '@capacitor/core'
 import {
   SignInWithApple,
@@ -23,7 +23,7 @@ import {
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   email: string = '';
   password: string = '';
   rememberMe: boolean = false;
@@ -32,9 +32,7 @@ export class LoginComponent implements OnInit {
   token: any = null;
   @Output() loginChanged = new EventEmitter<boolean>();
 
-  FACEBOOK_PERMISSIONS = [
-    'email',
-  ];
+  FACEBOOK_PERMISSIONS = ['email'];
 
   constructor(
     public commonService: CommonService,
@@ -44,12 +42,7 @@ export class LoginComponent implements OnInit {
     private loaderService: LoaderService,
     private loginService: LoginService,
     private http: HttpClient
-  ) {
-
-  }
-
-  ngOnInit() {
-  }
+  ) {}
 
   showRegisterPage() {
     this.loginChanged.emit(false);
@@ -75,77 +68,84 @@ export class LoginComponent implements OnInit {
       error: (err) => {
         this.loginSpinner = false;
         this.errorMessage = err.message;
-        this.toasterService.presentToast(err.error[0], 'warning')
-
+        this.toasterService.presentToast(err.error[0], 'warning');
       },
     });
   }
 
   async googleLogin() {
-    this.loaderService.showLoader()
-    GoogleAuth.signIn().then(r => {
-      const user = r
-      let dataToSend = {
-        email: user.email,
-        provider: 'Google',
-        idToken: user.authentication.idToken,
-      };
-      this.errorMessage = user.email;
-      this.loginService.googleLoginIn(dataToSend).subscribe({
-        next: (r) => {
-          const decodedToken = jwtDecode(r.accessToken);
-          // @ts-ignore
-          this.localStorageService.setUserEmail(decodedToken.email);
-          this.localStorageService.setUserToken(r.accessToken);
-          this.localStorageService.setUserRefreshToken(r.refreshToken);
-          this.localStorageService.setIsFromFacebookLoggedIn(false);
-          this.loaderService.hideLoader()
-          setTimeout(() => {
-            this.commonService.goToRoute('home');
-          }, 200);
-        },
-        error: (err) => {
-          this.toasterService.presentToast(err.error[0], 'warning')
-          this.errorMessage = err.message;
-          this.loaderService.hideLoader()
-        },
-      });
-    }, (e: any) => {
-      this.loaderService.hideLoader()
-    })
-  }
-
-  async facebookLogin() {
     this.loaderService.showLoader();
-
-    await FacebookLogin.login({permissions: this.FACEBOOK_PERMISSIONS}).then(result => {
-      if (result?.accessToken) {
-        this.loginService.facebookLoginIn(result.accessToken.token).subscribe({
+    GoogleAuth.signIn().then(
+      (r) => {
+        const user = r;
+        let dataToSend = {
+          email: user.email,
+          provider: 'Google',
+          idToken: user.authentication.idToken,
+        };
+        this.errorMessage = user.email;
+        this.loginService.googleLoginIn(dataToSend).subscribe({
           next: (r) => {
             const decodedToken = jwtDecode(r.accessToken);
             // @ts-ignore
             this.localStorageService.setUserEmail(decodedToken.email);
             this.localStorageService.setUserToken(r.accessToken);
             this.localStorageService.setUserRefreshToken(r.refreshToken);
-            this.localStorageService.setIsFromFacebookLoggedIn(true);
-            this.loaderService.hideLoader()
-
+            this.localStorageService.setIsFromFacebookLoggedIn(false);
+            this.loaderService.hideLoader();
             setTimeout(() => {
               this.commonService.goToRoute('home');
             }, 200);
           },
           error: (err) => {
-            this.loaderService.hideLoader()
+            this.toasterService.presentToast(err.error[0], 'warning');
             this.errorMessage = err.message;
-            this.toasterService.presentToast(err.error[0], 'warning')
+            this.loaderService.hideLoader();
           },
         });
-      } else {
-        this.loaderService.hideLoader()
+      },
+      (e: any) => {
+        this.loaderService.hideLoader();
       }
-    }, (e: any) => {
-      this.loaderService.hideLoader()
-    })
+    );
+  }
+
+  async facebookLogin() {
+    this.loaderService.showLoader();
+
+    await FacebookLogin.login({ permissions: this.FACEBOOK_PERMISSIONS }).then(
+      (result) => {
+        if (result?.accessToken) {
+          this.loginService
+            .facebookLoginIn(result.accessToken.token)
+            .subscribe({
+              next: (r) => {
+                const decodedToken = jwtDecode(r.accessToken);
+                // @ts-ignore
+                this.localStorageService.setUserEmail(decodedToken.email);
+                this.localStorageService.setUserToken(r.accessToken);
+                this.localStorageService.setUserRefreshToken(r.refreshToken);
+                this.localStorageService.setIsFromFacebookLoggedIn(true);
+                this.loaderService.hideLoader();
+
+                setTimeout(() => {
+                  this.commonService.goToRoute('home');
+                }, 200);
+              },
+              error: (err) => {
+                this.loaderService.hideLoader();
+                this.errorMessage = err.message;
+                this.toasterService.presentToast(err.error[0], 'warning');
+              },
+            });
+        } else {
+          this.loaderService.hideLoader();
+        }
+      },
+      (e: any) => {
+        this.loaderService.hideLoader();
+      }
+    );
   }
 
   appleLogin() {
@@ -163,28 +163,33 @@ export class LoginComponent implements OnInit {
       state: '12345',
       nonce: 'nonce',
     };
+
     SignInWithApple.authorize(options)
       .then((result: SignInWithAppleResponse) => {
         console.log('result: ', result);
-        this.doAppleLogin(result.response.identityToken,result.response.email);
+        let dataToSend = {
+          email: result.response.email,
+          identityToken: result.response.identityToken,
+        };
+        this.doAppleLogin(dataToSend);
         // Handle user information
         // Validate token with server and create new session
       })
-      .catch(error => {
+      .catch((error) => {
         // Handle error
       });
   }
 
-  doAppleLogin(token: any,email: any) {
+  doAppleLogin(data: any) {
     this.loaderService.showLoader();
-    this.loginService.appleLogin(token,email).subscribe({
+    this.loginService.appleLogin(data).subscribe({
       next: (r) => {
         const decodedToken = jwtDecode(r.accessToken);
         // @ts-ignore
         this.localStorageService.setUserEmail(decodedToken.email);
         this.localStorageService.setUserToken(r.accessToken);
         this.localStorageService.setUserRefreshToken(r.refreshToken);
-        this.loaderService.hideLoader()
+        this.loaderService.hideLoader();
 
         setTimeout(() => {
           this.commonService.goToRoute('home');
@@ -193,7 +198,7 @@ export class LoginComponent implements OnInit {
       error: (err) => {
         this.loaderService.hideLoader();
         this.errorMessage = err.message;
-        this.toasterService.presentToast(err.error[0], 'warning')
+        this.toasterService.presentToast(err.error[0], 'warning');
       },
     });
   }
