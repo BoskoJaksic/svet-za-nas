6f4154ee6874c91f5d1eec466b9a96bf0384ba17
boskojaksic <bosko.jaksic@wedosoftware.eu>
2024-07-30 12:02:06 +0200

    plist ušdated

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -19,6 +19,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
             return true
         }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+      Messaging.messaging().apnsToken = deviceToken
+      Messaging.messaging().token(completion: { (token, error) in
+        if let error = error {
+            NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
+        } else if let token = token {
+            NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: token)
+        }
+      })
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+      NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
+    }
 
 
     func applicationWillResignActive(_ application: UIApplication) {
